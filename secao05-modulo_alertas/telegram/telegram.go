bfa9838cdd94726b2d07c2a5cf0af9190a9db916
felippemozer <felippemozer22@gmail.com>
2023-09-07 16:47:16 -0300

    feat: create telegram feature

--- secao05-modulo_alertas/telegram/telegram.go
+++ secao05-modulo_alertas/telegram/telegram.go
@@ -1,1 +1,35 @@
 package telegram
+
+import (
+	"os"
+	"strconv"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+)
+
+type Message struct {
+	Text    string `json:"text"`
+	GroupId int64  `json:"group_id"`
+}
+
+func SendTelegram(apiKey string, textMessage string) {
+	bot, err := tgbotapi.NewBotAPI(apiKey)
+	if err != nil {
+		panic(err)
+	}
+	message := Message{}
+	message.Text = textMessage
+
+	groupId := os.Getenv("TELEGRAM_CHANNEL_ID")
+	if groupId == "" {
+		panic("TELEGRAM_GROUP_ID não foi configurado")
+	}
+
+	message.GroupId, err = strconv.ParseInt(groupId, 10, 64)
+	if err != nil {
+		panic(err)
+	}
+	bot.Debug = true
+	alertText := tgbotapi.NewMessage(message.GroupId, message.Text)
+	bot.Send(alertText)
+}
