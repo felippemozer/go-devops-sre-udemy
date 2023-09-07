1be5e52e408a2234d1c0169a6fc9fd4a6f9fcc4f
felippemozer <felippemozer22@gmail.com>
2023-09-07 16:06:34 -0300

    feat: create sms feature

--- secao05-modulo_alertas/sms/sms.go
+++ secao05-modulo_alertas/sms/sms.go
@@ -1,1 +1,58 @@
 package sms
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func SendSMS(message string, phone string) {
+
+	apiKey := os.Getenv("VONAGE_API_KEY")
+	if apiKey == "" {
+		panic("VONAGE_API_KEY não foi setado")
+	}
+
+	apiSecret := os.Getenv("VONAGE_API_SECRET")
+	if apiSecret == "" {
+		panic("VONAGE_API_SECRET não foi setado")
+	}
+
+	data := url.Values{}
+	data.Set("api_key", apiKey)
+	data.Set("api_secret", apiSecret)
+	data.Set("from", "Vonage APIs")
+	data.Set("text", message)
+	data.Set("to", phone)
+	fmt.Println(data.Encode())
+
+	endpoint := "https://rest.nexmo.com/sms/json"
+	r, err := http.NewRequest("POST", endpoint, strings.NewReader(data.Encode()))
+	if err != nil {
+		panic(fmt.Sprintf("Request not allowed. See error below\n%s", err))
+	}
+
+	r.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	r.Header.Add("Content-Length", strconv.Itoa(len(data.Encode())))
+
+	client := &http.Client{}
+	res, err := client.Do(r)
+	if err != nil {
+		panic(fmt.Sprintf("Error on URL invoke. See error below\n%s", err))
+	}
+	defer res.Body.Close()
+
+	log.Println(res.Status)
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		panic(fmt.Sprintf("Error when read response body. See error below\n%s", err))
+	}
+
+	fmt.Println(string(body))
+
+}
